2c4f05fa99fe8161ca636e70b7210b0247ef63b8
Simon Osborne <SRC_Osborne1992@hotmail.co.uk>
2015-10-13 21:17:44 +0100

    Removing comments from building

--- building.rb
+++ building.rb
@@ -1,7 +1,7 @@
 class Building
   attr_accessor :name, :post_code, :floors, :occupants
   attr_reader :lifts
-  # methods: people occupation?
+
   def initialize(options={})
     self.name = options[:name]
     self.post_code = options[:post_code]
