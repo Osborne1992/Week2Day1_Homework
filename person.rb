bb80f39d6399b7b2240219c485a717bfbd0dfca2
Simon Osborne <SRC_Osborne1992@hotmail.co.uk>
2015-10-13 21:22:38 +0100

    Attempting to alter person file for floor tracking

--- person.rb
+++ person.rb
@@ -1,12 +1,12 @@
 class Person
   attr_accessor :name, :age, :destination
-  attr_reader :current_floor
+  attr_reader :floor
 
   def initialize(options={})
     self.name = options[:name]
     self.age = options[:age]
     self.destination = options.fetch :destination
-    self.current_floor = options.fetch :current_floor
+    self.floor = options.fetch :floor
   end
 
 end
