9e6b6910f1248a30e526e02d58dcea72af3c573f
Simon Osborne <SRC_Osborne1992@hotmail.co.uk>
2015-10-13 21:13:29 +0100

    Adding new people to main

--- main.rb
+++ main.rb
@@ -19,6 +19,8 @@ robby     = Person.new(name: 'Robby', age: 10, floor: 0, destination: 3)
 rob       = Person.new(name: 'Rob', age: 20, floor: 0, destination: 5)
 robert    = Person.new(name: 'Robert', age: 35, floor: 0, destination: 8)
 bob       = Person.new(name: 'Bob', age: 63, floor: 0, destination: 12)
+simon     = Person.new(name: 'Simon', age: 23, floor 0, destination: 10)
+john      = Person.new(name: 'John', age: 26, floor 0, destination: 11)
 
 
 binding.pry;''
